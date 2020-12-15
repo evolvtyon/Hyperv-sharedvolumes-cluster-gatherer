62fc6ecba143ffd8367a92518e7df5f2b6d0d479
Francisco Ramos <franciscoacramos@gmail.com>
2020-12-15 20:19:28 +0000

    Update Gather-all-cluster-shrVolumes.ps1

--- Gather-all-cluster-shrVolumes.ps1
+++ Gather-all-cluster-shrVolumes.ps1
@@ -5,14 +5,15 @@ Function Gather-all-cluster-shrVolumes {
     [string]$ClusterName
     )
 
-    $volume = Get-ClusterSharedVolume -Cluster $ClusterName;
-    % { $volume | `
-    Add-Member -NotePropertyName “Name” -ExpandPropertyValue $_.SharedVolumeInfo; $volume; } `
-    Select-Object Name, `
-                 @{Label = 'Path';expression={$_.FriendlyVolumeName}}, `
-                 @{Label = 'Size(GB)';expression={$_.Partition.Size/1gb -as [int]}}, `
-                 @{Label = 'Free Space(GB)';expression={$_.Partition.FreeSpace/1gb -as [int]}}, `
-                 @{Label = 'Percentage Free(%)';expression={$_.Partition.PercentFree}} | `
-    Export-Csv -Path "C:\$($ClusterName)-Clustervolumes.csv" -NoTypeInformation
-            
+
+    $volumes = Get-ClusterSharedVolume -Cluster $ClusterName;
+
+    %{ $volume = $volumes | select -Property "Name" -ExpandProperty "SharedVolumeInfo"; $volume | `
+         Add-Member -NotePropertyName "Path" -NotePropertyValue $_.FriendlyVolumeName; $volume; } | `
+         Select-Object Name, `
+                      @{Label = 'Path';expression={$_.FriendlyVolumeName}}, ` 
+                      @{Label = 'Size(GB)';expression={$_.Partition.Size/1gb -as [int]}}, ` 
+                      @{Label = 'Free Space(GB)';expression={$_.Partition.FreeSpace/1gb -as [int]}}, ` 
+                      @{Label = 'Percentage Free(%)';expression={$_.Partition.PercentFree}} | ` 
+         Export-Csv -Path "C:\$($ClusterName)-Clustervolumes.csv" -NoTypeInformation 
 }
